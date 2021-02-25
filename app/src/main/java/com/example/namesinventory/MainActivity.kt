df88c7f7dae2618f46736a076c67ad3984e36538
Kirubanand <prdkiruba@gmail.com>
2021-02-25 19:59:07 +0530

    Add name option added

--- app/src/main/java/com/example/namesinventory/MainActivity.kt
+++ app/src/main/java/com/example/namesinventory/MainActivity.kt
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.names_list_layout.*
 import kotlinx.android.synthetic.main.names_list_layout.view.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,19 +14,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val names = arrayListOf<String>()
-
-        names.add("Kiruba R")
-
-        listViewOfNames.adapter = ArrayAdapter<String> (
-            this ,
-            R.layout.names_list_layout ,
-            R.id.nameTextView ,
-            names
-        )
-
-        listViewOfNames.setOnItemClickListener{ parent , view , position , id ->
-            Toast.makeText(this , "Your name is: ${view.nameTextView.text}", Toast.LENGTH_SHORT).show()
+        var name: String
+        var arrayList = ArrayList<String>()
+        button.setOnClickListener {
+            name = editText.text.toString()
+            listViewOfNames.adapter = ArrayAdapter<String>(this,
+                    R.layout.names_list_layout,
+                    R.id.nameTextView,
+                    arrayList
+            )
+            if(name.isNotEmpty())
+                arrayList.add(name)
+            else
+                Toast.makeText(this , "Text is empty", Toast.LENGTH_SHORT).show()
         }
     }
 }
